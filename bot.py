e45ce7e8c34978801a3b0df151001be16d55aa31
WARRIORS <56763972+ccadmin1@users.noreply.github.com>
2022-02-28 11:35:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -156,7 +156,7 @@ async def new_Doodstream_url(urls):
 
 async def remove_username(new_List):
     for i in new_List:
-        if('@' in i or 't.me' in i or 'https://bit.ly/abcd' in i or 'https://bit.ly/123abcd' in i or 'telegra.ph' in i or 'https://youtu.be/abcd' in ):
+        if('@' in i or 't.me' in i or 'https://bit.ly/abcd' in i or 'https://bit.ly/123abcd' in i or 'telegra.ph' in i or 'https://youtu.be/abcd' in i or 'http://mdisk.fun/QuBXzL' in i or 'https://t.me/' in):
             new_List.remove(i)
     return new_List
 
